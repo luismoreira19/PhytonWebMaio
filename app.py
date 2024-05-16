f563d83e5a3c606e0773db8df82ae529c935ec35
luismoreira19 <lm167227@gmail.com>
2024-05-16 00:11:11 +0000

    Atualização dos códigos

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("Olá")
 
 @app.route('/')
 def ola():
-    return "Olá Mundo"
+    return render_template ('ola.html')
